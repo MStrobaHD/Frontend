1944eb13777a1cb251dd58738a2bf171bdbfac06
MateuszStroba <mateusz.stroba@student.wat.edu.pl>
2020-05-22 01:11:45 +0200

    asd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://education-support-app-api.azurewebsites.net/api/'
+  apiUrl: 'https://education-support-app-api.azurewebsites.net/api/'
 };
